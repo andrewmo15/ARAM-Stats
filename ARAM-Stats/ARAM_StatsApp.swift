bce0071adcfb68c14238fc285f832976a8cafa29
Andrew Mo <andrewmo15@Andrews-MacBook-Pro.local>
2023-09-18 17:17:19 -0500

    first commit

---
+++ ARAM-Stats/ARAM_StatsApp.swift
@@ -0,0 +1,20 @@
+//
+//  ARAM_StatsApp.swift
+//  ARAM-Stats
+//
+//  Created by Andrew Mo on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ARAM_StatsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
